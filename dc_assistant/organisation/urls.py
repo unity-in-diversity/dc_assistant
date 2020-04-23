9556eaff2e604b5fad8e7156dbcf36ec6069d2f2
Maxim D <maxim.dekin@gmail.com>
2020-04-23 22:53:22 +0300

    added model inheritance for loggin changes

--- dc_assistant/organisation/urls.py
+++ dc_assistant/organisation/urls.py
@@ -6,7 +6,7 @@ app_name = 'organisation'
 
 urlpatterns = [
     path('regions/', views.region_view, name='region_list'),
-    path('regions/add', views.region_add, name='region_add'),
+    path('regions/add', views.RegionAdd.as_view(), name='region_add'),
     path('locations/', views.location_view, name='location_list'),
     path ('locations/add', views.LocationAdd.as_view(), name='location_add')
 ]
--- dc_assistant/organisation/views.py
+++ dc_assistant/organisation/views.py
@@ -6,7 +6,6 @@ from django.urls import reverse, reverse_lazy
 from django.views.generic import View, CreateView
 # Create your views here.
 
-
 def region_view(request):
     regions = Region.objects.all()
     return render(request, 'organisation/regions.html', context={'regions': regions})
@@ -15,24 +14,11 @@ def location_view(request):
     locations = Location.objects.all()
     return render(request, 'organisation/locations.html', context={'locations': locations})
 
-
-def region_add(request):
-    if request.method == 'POST':
-        form = RegionAddForm(request.POST)
-        if form.is_valid():
-             new_region = Region(
-                 parent=form.cleaned_data['parent'],
-                 name=form.cleaned_data['name'],
-                 slug=form.cleaned_data['slug'],
-             )
-             new_region.save()
-             return HttpResponseRedirect(reverse('organisation:region_list'))
-        else:
-            form = RegionAddForm()
-            return render(request, 'organisation/region_add.html', context={'form': form})
-    else:
-        form = RegionAddForm()
-        return render(request, 'organisation/region_add.html', context={'form': form})
+class RegionAdd(CreateView):
+    form_class = RegionAddForm
+    model = Region
+    success_url = reverse_lazy('organisation:region_list')
+    template_name = 'organisation/region_add.html'
 
 class LocationAdd(CreateView):
     form_class = SiteAddForm
