936c57b0b5daefc59a000f174e13d429b3d60b5c
maxim <dekin_m@mail.ru>
2020-05-14 16:44:51 +0300

    added rack add form, html templ, one view created by django tables2 with custom html template for render

--- dc_assistant/organisation/forms.py
+++ dc_assistant/organisation/forms.py
@@ -50,19 +50,14 @@ class LocationAddForm(forms.ModelForm):
 class RackAddForm(forms.ModelForm):
     name = forms.CharField(label='Rack Label', widget=forms.TextInput(attrs={'class': 'form-control'}))
     location = forms.ModelChoiceField(queryset=Location.objects.all(), empty_label="------", widget=forms.Select(attrs={'class': 'select2 form-control'}))
-    desc_units = forms.BooleanField(label='Top to buttom', widget=forms.CheckboxInput(attrs={'class': 'col-auto ml-2'}))
+    u_height = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    desc_units = forms.BooleanField(label='Top to buttom', widget=forms.CheckboxInput(attrs={'class': 'form-control col-md-1'}))
     comment = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Rack
         fields = ['name', 'location', 'u_height', 'desc_units', 'racktype', 'comment',
         ]
-        # help_texts = {
-        #     'location': "The site at which the rack exists",
-        #     'name': "Organisational rack name",
-        #     'u_height': "Height in rack units",
-        #     'desc_units': 'Ordering of units',
-        # }
         widgets = {
             'racktype': StaticSelectWidget()
         }
